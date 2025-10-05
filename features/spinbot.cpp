7b61106ddc2dfa0e2adf6a2345239a1d980e1e36
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-10-05 14:34:44 +0200

    remove spinbot and mouse glitch (broken thanks to vanguard)

--- features/spinbot.cpp
+++
@@ -1,9 +0,0 @@
-#include "spinbot.h"
-#include "../core/input.h"
-
-void runSpinbot(const SpinbotConfig& cfg, FeatureRunner<SpinbotConfig>& runner) {
-	runner.run(cfg, [&cfg]() {
-		int dx = (cfg.direction == "left" ? -cfg.speed : cfg.speed);
-		moveMouse(dx, 0);
-		}, "Spinbot");
-}
